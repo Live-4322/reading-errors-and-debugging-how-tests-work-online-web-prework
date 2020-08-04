6d414c5f622b0723ce2539ed4abd61dbba3da945
Charlie Oster <oster.charlie@yahoo.com>
2020-08-04 20:23:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 first_number = 1  
 
 second_number = 2 
@@ -13,3 +14,42 @@ product = first_number * second_number
 mod = first_number % second_number
 
 
+=======
+def addition(first_number, second_number)
+sum = first_number + second_number
+  
+end
+
+def subtraction(first_number, second_number)
+  
+difference = first_number - second_number
+
+end
+
+def division(first_number, second_number)
+  
+quotient = first_number / second_number
+
+end
+
+def multiplication(first_number, second_number)
+  
+product = first_number * second_number
+
+end
+
+def modulo(first_number, second_number)
+  
+mod = first_number % second_number
+
+end
+
+def square_root(num)
+  
+num = 81
+
+Math.sqrt.(num)
+
+
+end
+>>>>>>> c5eacbd2029999fea89c8a80a5604b9f61684996
